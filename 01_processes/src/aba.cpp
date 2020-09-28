b8c47c7d9d2cfe3cf2ea0e15bdef48015823870a
SandnerPascal <passan41@gmail.com>
2020-09-28 19:10:58 +0200

    Nummer 9 - charout in aba benutzen

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -9,27 +9,27 @@
 #include <thread>
 #include <csignal>
 #include <cstdlib>
+#include <cerrno>
+
 
 using namespace std;
 
 
-int main() {
+int main(int argc, char const* argv[]) {
     auto pid{fork()};
-    int count = 1;
+    //int count = 1;
     while(true){              
         if(pid > 0) {
             cout << "B" << flush;
             chrono::milliseconds sleeptime(500);
             this_thread::sleep_for(sleeptime);
-            if(count == 6){
-                kill(pid, SIGKILL);
-                quick_exit(EXIT_SUCCESS);
-            }
+            //if(count == 6){
+            //    kill(pid, SIGKILL);
+            //    quick_exit(EXIT_SUCCESS);
+            //}
         }else if(pid == 0){
-            cout << "A" << flush;
-            chrono::milliseconds sleeptime(500);
-            this_thread::sleep_for(sleeptime);         
+            execl("charout", "charout", "G", nullptr);        
         }
-        count ++;
+        //count ++;
     }
 }
