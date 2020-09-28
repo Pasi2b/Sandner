09cb0deae2586546c48d6b2c796aafc61ec1a933
SandnerPascal <passan41@gmail.com>
2020-09-28 18:27:49 +0200

    Nummer 6 - Kind Prozess wird von Elternprozess nach 3 Sekunden beendet und beendet sich dannach selber

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -7,22 +7,29 @@
 #include <unistd.h>
 #include <chrono>
 #include <thread>
-
+#include <csignal>
+#include <cstdlib>
 
 using namespace std;
 
 
 int main() {
     auto pid{fork()};
-    while(true){        
+    int count = 1;
+    while(true){              
         if(pid > 0) {
             cout << "B" << flush;
             chrono::milliseconds sleeptime(500);
             this_thread::sleep_for(sleeptime);
+            if(count == 6){
+                kill(pid, SIGKILL);
+                quick_exit(EXIT_SUCCESS);
+            }
         }else if(pid == 0){
             cout << "A" << flush;
             chrono::milliseconds sleeptime(500);
-            this_thread::sleep_for(sleeptime);
+            this_thread::sleep_for(sleeptime);         
         }
+        count ++;
     }
 }
