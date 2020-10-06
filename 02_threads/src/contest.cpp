1698f8fae6b14a8fdcc24c2ec2b577b2a74ff588
SandnerPascal <passan41@gmail.com>
2020-10-06 18:35:11 +0200

    Nummer 5 - Zufällige rundenzeit wird hinzugefügt

--- 02_threads/src/contest.cpp
+++ 02_threads/src/contest.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <thread>
+#include <random>
 
 using namespace std;
 using namespace std::literals;
@@ -15,13 +16,17 @@ using namespace std::literals;
 class Car{
     private: 
         string autotyp;
+        
     public:
         void operator()(){
+            random_device rd;
+            mt19937 gen{rd()};
+            uniform_real_distribution<> dis{1, 10};
             int runden = 0;
 
             while(true){
                 runden++;
-            cout << to_string(runden) << " " << autotyp << "\n" << flush;
+            cout << to_string(runden) << " " << autotyp << " " << dis(gen) << "\n" << flush;
                 this_thread::sleep_for(1s);   
             }
         }
@@ -30,23 +35,27 @@ class Car{
     }
 };
 
-/*
+
 void rennen(){
-    int runden = 0;
-    string autotyp = "Toyota GT86";
+    int runden2 = 0;
+    string autotyp2 = "Toyota GT86";
+    random_device rd;
+    mt19937 gen{rd()};
+    uniform_real_distribution<> dis{1, 10};
 
     while(true){
-        runden++;
-        cout << runden << " " << autotyp << endl;
-        cout << flush;
+        runden2++;
+        cout << to_string(runden2) << " " << autotyp2 << " " << dis(gen) << "\n" << flush;
         this_thread::sleep_for(1s);   
     }
 }
-*/      
+     
 
 
 int main(){
     Car toyota("Toyota GT86") ;   
     thread t1{ref(toyota)};
+    thread t2{rennen};
     t1.join();
+    t2.join();
 }
