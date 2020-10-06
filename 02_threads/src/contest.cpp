b60757fb7073ea63d5e83c2b104a39f294a85493
SandnerPascal <passan41@gmail.com>
2020-10-06 18:51:54 +0200

    Nummer 5 - der thread schläft jetz so lang wie die random rundenzeit ist

--- 02_threads/src/contest.cpp
+++ 02_threads/src/contest.cpp
@@ -7,9 +7,11 @@
 #include <iostream>
 #include <thread>
 #include <random>
+#include <iomanip>
 
 using namespace std;
 using namespace std::literals;
+using namespace chrono;
 
 
 
@@ -23,11 +25,13 @@ class Car{
             mt19937 gen{rd()};
             uniform_real_distribution<> dis{1, 10};
             int runden = 0;
+            double time{};
 
             while(true){
+                time = dis(gen);
                 runden++;
-            cout << to_string(runden) << " " << autotyp << " " << dis(gen) << "\n" << flush;
-                this_thread::sleep_for(1s);   
+                cout << to_string(runden) << " " << autotyp << " " << dis(gen) << "\n" << flush;
+                this_thread::sleep_for(milliseconds(static_cast<int>(time * 1000)));   
             }
         }
     Car(string Autotyp){
@@ -38,15 +42,18 @@ class Car{
 
 void rennen(){
     int runden2 = 0;
-    string autotyp2 = "Toyota GT86";
+    string autotyp2 = "Toyota Supra";
     random_device rd;
     mt19937 gen{rd()};
     uniform_real_distribution<> dis{1, 10};
+    double time{};
+    
 
     while(true){
+        time = dis(gen);
         runden2++;
         cout << to_string(runden2) << " " << autotyp2 << " " << dis(gen) << "\n" << flush;
-        this_thread::sleep_for(1s);   
+        this_thread::sleep_for(milliseconds(static_cast<int>(time * 1000)));   
     }
 }
      
