e9b08e967a82e1b5d7a8324a2273d090c4264d05
SandnerPascal <passan41@gmail.com>
2020-10-06 09:04:19 +0200

    Einrichten der Übung 2

---
+++ 02_threads/src/contest.cpp
@@ -0,0 +1,14 @@
+/*
+    author: Pascal Sandner
+    date:   05.10.2020
+    desc:   02_threads
+*/
+
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int main(/*int argc, char *argv[]*/){
+    cout << "hallo" << endl;
+}
