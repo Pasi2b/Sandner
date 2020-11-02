56f74027745286f47483238bca9c02f639c8781b
SandnerPascal <passan41@gmail.com>
2020-11-02 18:28:44 +0100

    Nummer 2 - zwei threads greifen jetzt auf die balance zu und das ergebnis ist wie gewünscht

--- 03_sync/src/main.cpp
+++ 03_sync/src/main.cpp
@@ -6,10 +6,12 @@
 
 #include <iostream>
 #include "include/account.h"
+#include <thread>
 
 using namespace std;
 
 int main(){
+    /* Punkt1
     Account acc{15};
     cout << acc.get_balance();
     cout << "\n";
@@ -19,4 +21,15 @@ int main(){
     acc.withdraw(10);
     cout << acc.get_balance();
     cout << "\n";
+    */
+
+    Account acc2{1};
+    cout << acc2.get_balance();
+    cout << "\n";
+    thread t1([ref(acc2.withdraw(1))](){});
+    thread t2([ref(acc2.withdraw(1))](){});
+    t1.join();
+    t2.join();
+    cout << "\n";
+    cout << acc2.get_balance();
 }
