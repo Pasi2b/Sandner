2093c95149d2d6b42ab3619cf127a1f15d2a098b
SandnerPascal <passan41@gmail.com>
2020-12-30 13:40:27 +0100

    Nummer 5 - Worker wartete jetz für eine Random Zeit, Boss wartet ebenfalls für eine Random Zeit

--- 04_sync2/src/loadsim.cpp
+++ 04_sync2/src/loadsim.cpp
@@ -7,21 +7,28 @@
 #include <thread>
 #include <mutex>
 #include <queue>
+#include <random>
 
 using namespace std;
 
 
 void worker (int id, WorkQueue& q){
     string output;
+    double wait_worker;
+    random_device rd;
+    mt19937 gen{rd()};
+    uniform_real_distribution<> dis{1, 10};
 
     while(true){   
         output =  "W" + to_string(id) + ": Want work packet \n";
         cout << output << flush;
         WorkPacket WP{q.pop()};
+        wait_worker = dis(gen);
+        chrono::milliseconds time_to_wait_worker{int (wait_worker * 1000)};
         output =  "W" + to_string(id) + ": Got work packet " + to_string(WP.get_id()) + "\n";
         cout << output << flush;
-        this_thread::sleep_for(1s);
-        output + "W" + to_string(id) + ": Processed work packet " + to_string(WP.get_id()) + "\n";
+        this_thread::sleep_for(time_to_wait_worker);
+        output = "W" + to_string(id) + ": Processed work packet " + to_string(WP.get_id()) + " (" + to_string(wait_worker) + ")" + "\n";
         cout << output << flush;
     }
 }
@@ -35,15 +42,25 @@ int main() {
 
     thread t1{worker, 1, ref(q)};
     thread t2{worker, 2, ref(q)};
+    thread t3{worker, 3, ref(q)};
+
+    double wait_boss;
+    random_device rd;
+    mt19937 gen{rd()};
+    uniform_real_distribution<> dis{0, 1};
+
 
     while(true){
         WorkPacket WP{WorkPacket(i)};
         q.push(WP);
-        cout << "B: Submitted work packet " << i << endl;
-        this_thread::sleep_for(0.5s);
+        wait_boss = dis(gen);
+        chrono::milliseconds time_to_wait_boss{int (wait_boss * 1000)};
+        cout << "B: Submitted work packet " << i << " [" << to_string(wait_boss) << "]" << endl;
+        this_thread::sleep_for(time_to_wait_boss);
         i++;
     }
 
     t1.join();
     t2.join();
+    t3.join();
 }
