8d62d8a2737741d7c3b1a912e81371ac07f69eb2
SandnerPascal <passan41@gmail.com>
2020-12-31 19:46:57 +0100

    Nummer 4 - Implementieren und verwendne der klasse timeslave

--- 07_berkley/src/main.cpp
+++ 07_berkley/src/main.cpp
@@ -12,15 +12,29 @@ using namespace std;
 
 
 
+class TimeSlave{
+    private:
+        string name;
+        Clock clock;
 
+    public:
+        TimeSlave(string name_, int hours_, int minutes_, int seconds_): clock(name_ + " clock", hours_, minutes_, seconds_){
+            name = name_;
+        }
+
+        void operator()(){
+            clock();
+        }
+};
 
 
 int main(){
 
-    thread t1(Clock{"testclock"});
-    thread t2(Clock{"testclock", 12, 00, 00});
-    t1.join();
-    t2.join();
+    //thread t1(Clock{"testclock"});
+    //thread t2(Clock{"testclock", 12, 00, 00});
+    //t1.join();
+    //t2.join();
 
-    
+    thread t1(TimeSlave{"testslave", 12, 00, 00});
+    t1.join();
 }
