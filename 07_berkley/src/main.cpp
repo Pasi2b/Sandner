6377479c7737dbe6f64fc579ba144c8b0e83b9c2
SandnerPascal <passan41@gmail.com>
2020-12-31 19:54:48 +0100

    Nummer 6 - Implementieren einer timemaster klasse analog zur timeslave klasse

--- 07_berkley/src/main.cpp
+++ 07_berkley/src/main.cpp
@@ -27,6 +27,21 @@ class TimeSlave{
         }
 };
 
+class TimeMaster{
+    private:
+        string name;
+        Clock clock;
+
+    public:
+        TimeMaster(string name_, int hours_, int minutes_, int seconds_): clock(name_ + " clock", hours_, minutes_, seconds_){
+            name = name_;
+        }
+
+        void operator()(){
+            clock();
+        }
+};
+
 
 int main(){
 
@@ -36,7 +51,9 @@ int main(){
     //t2.join();
 
     thread slave1(TimeSlave{"testslave", 12, 00, 00});
-    thread slave2(TimeSlave{"testslave2", 10, 00, 00});
+    thread slave2(TimeSlave{"testslave2", 11, 00, 00});
+    thread timemaster1(TimeMaster{"testmaster", 10, 00, 00});
     slave1.join();
     slave2.join();
+    timemaster1.join();
 }
