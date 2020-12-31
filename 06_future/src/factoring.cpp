7d197604b734cb61f2892f5d2cc7d5464adeb4d6
SandnerPascal <passan41@gmail.com>
2020-12-31 13:15:22 +0100

    Nummer 2 - calc_factors wird eingebunden und ergebnis wird berechnet und ausgegeben

--- 06_future/src/factoring.cpp
+++ 06_future/src/factoring.cpp
@@ -7,11 +7,14 @@
 #include <vector>
 #include <thread>
 #include "CLI11.hpp"
+#include "calc_factors.h"
+
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmisleading-indentation"
 #pragma GCC diagnostic ignored "-Wsign-compare"
 #include "InfInt.h"
 #pragma GCC diagnostic pop
+
 
 
 
@@ -21,12 +24,11 @@ using namespace std;
 string checkValidator(const string& s){
     std::size_t found = s.find_first_not_of("0123456789");
     if (found != string::npos){
-         throw CLI::ValidationError(s +" contains not numeric character");
+         throw CLI::ValidationError(s + " contains not numeric character");
     }else{
         return ""; 
     }
 }
-
 
 
 int main(int argc, char const *argv[]){
@@ -34,9 +36,29 @@ int main(int argc, char const *argv[]){
 
     CLI::App app("Factor numbers");
     app.add_option("number", inputs, "numbers to factor")
+        ->required()
         ->check(checkValidator);
     bool asy{false};
     app.add_flag("-a, --async", asy, "async");
     CLI11_PARSE(app, argc, argv);
     
+    try{
+            app.parse(argc, argv);
+            vector<InfInt> newinput;
+            for(size_t i = 0; i < inputs.size(); i++){
+                InfInt input_item = inputs[i];
+                newinput.push_back(input_item);
+                //cout << input_item << "\n" << flush;
+            }
+            for(size_t i = 0; i < newinput.size(); i++){
+                vector<InfInt> factors = get_factors(newinput[i]);
+                cout << newinput[i] << ": " << flush;
+                for(size_t j = 0; j < factors.size(); j++){
+                    cout << factors[j] << " " << flush;
+                }
+                cout << "\n" << flush;
+            }
+    }catch (const CLI::ParseError &error){
+        return app.exit(error);
+    }
 }
